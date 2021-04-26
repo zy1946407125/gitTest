da9c846da1de6dffaa757d521192d2be9c2ef63e
邹渊 <1946407125@qq.com>
2021-04-26 20:25:07 +0800

    gitee pull commit

--- src/main/java/com/magic233/GitTest.java
+++ src/main/java/com/magic233/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("gitee test");
+        System.out.println("gitee pull test");
     }
 }
