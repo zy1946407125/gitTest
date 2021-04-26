e1a9853b9d32ad9546cd4dcb36de8cc11291290c
zouyuan <1946407125@qq.com>
2021-04-26 19:11:19 +0800

    first commit

---
+++ src/main/java/com/magic233/GitTest.java
@@ -0,0 +1,13 @@
+package com.magic233;
+
+/**
+ * ClassName: GitTest
+ * Description:
+ * date: 2021/4/26 17:50
+ * author: zouyuan
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
