fc3c110359dbac0bc6933370afd86dbf12733a8d
zouyuan <1946407125@qq.com>
2021-04-26 19:14:34 +0800

    second commit

--- src/main/java/com/magic233/GitTest.java
+++ src/main/java/com/magic233/GitTest.java
@@ -9,5 +9,6 @@ package com.magic233;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git2");
     }
 }
